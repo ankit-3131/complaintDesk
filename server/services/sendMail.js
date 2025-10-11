fe9ce433fedc213bc7e067841c252e4ba88ce68f
ankit-3131 <heyyynkit@gmail.com>
2025-10-11 09:34:03 +0530

    Mail warning feature

--- server/services/sendMail.js
+++ server/services/sendMail.js
@@ -6,7 +6,7 @@
     const transporter = nodemailer.createTransport({
         service: "SendGrid",
         auth: {
-          user: "apikey", 
+          user: "apikey",
           pass: process.env.SENDGRID_API_KEY,
         },
       });
