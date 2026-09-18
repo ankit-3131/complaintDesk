873de3f53e8673954e73ac71e48f1ea540da2b9b
ankit-3131 <heyyynkit@gmail.com>
2026-09-18 10:21:43 +0530

    deployment url fixes

--- server/app.js
+++ server/app.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(cookieParser())
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://complaint-desk-client.vercel.app",
   "https://complaint-desk-sage.vercel.app"
 ];
 
